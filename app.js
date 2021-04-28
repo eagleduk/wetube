fed0b4879c47ddf3ae77d6cd25ab66ded2bb4c1d
eagleduk <eagleduk@gmail.com>
2021-04-28 17:29:51 +0900

    #3.10 MVC Pattern part Two
    URL 주소 리스트 작성

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
 /* Routers */
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -17,8 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
