56790c97ab294cf9f99748717ac8801437c5c61f
eagleduk <eagleduk@gmail.com>
2021-04-29 22:37:19 +0900

    #4.1 Connecting to MongoDB
    mongoDB connect

--- db.js
+++ db.js
@@ -1,67 +1,16 @@
-export const videos = [
-  {
-    id: 111,
-    title: "Video 1",
-    description: "Video Description 1",
-    views: 22,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 112,
-      name: "creator",
-      email: "creator@wetube.com",
-    },
-  },
-  {
-    id: 211,
-    title: "Video 2",
-    description: "Video Description 2",
-    views: 22,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 212,
-      name: "creator",
-      email: "creator@wetube.com",
-    },
-  },
-  {
-    id: 311,
-    title: "Video 3",
-    description: "Video Description 3",
-    views: 22,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 312,
-      name: "creator",
-      email: "creator@wetube.com",
-    },
-  },
-  {
-    id: 411,
-    title: "Video 4",
-    description: "Video Description 4",
-    views: 22,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 412,
-      name: "creator",
-      email: "creator@wetube.com",
-    },
-  },
-  {
-    id: 511,
-    title: "Video 5",
-    description: "Video Description 5",
-    views: 22,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 512,
-      name: "creator",
-      email: "creator@wetube.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connect to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,8 +1,8 @@
+import "./db";
 import app from "./app";
 
 const PORT = 4000;
 
-const handleListening = () =>
-  console.log(`Listening... ✅ http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
