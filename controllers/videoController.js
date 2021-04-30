a2a39afad4bc77a03dfca6e68ebff43cd71cbe1a
eagleduk <eagleduk@gmail.com>
2021-04-30 16:56:24 +0900

    #4.10 Deleting a Video
    video 삭제

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -47,7 +47,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -70,7 +70,6 @@ export const postEditVideo = async (req, res) => {
     params: { id },
     body: { title, description },
   } = req;
-  console.log(id, title, description);
   try {
     //await Video.findByIdAndUpdate(id, { title, description });
     await Video.findOneAndUpdate({ _id: id }, { title, description });
@@ -80,5 +79,17 @@ export const postEditVideo = async (req, res) => {
   }
 };
 
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  console.log(id);
+  try {
+    await Video.findOneAndRemove({ _id: id });
+    //await Video.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.redirect(routes.home);
+};
