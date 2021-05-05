1f406b69cfab95e95af7c89addf55e9d2988902e
eagleduk <eagleduk@gmail.com>
2021-05-05 22:10:29 +0900

    #9.2 Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,18 +2,32 @@ const videoContainer = document.querySelector("#wetube-player");
 const videoPlayer = videoContainer && videoContainer.querySelector("video");
 const playBtn =
   videoContainer && videoContainer.querySelector("#wetube-player__playBtn");
+const volumeBtn =
+  videoContainer && videoContainer.querySelector("#wetube-player__volumeBtn");
 
 function handleVideoPlay() {
-  console.log(videoPlayer.paused);
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = `<i class="fas fa-pause"></i>`;
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+  }
+}
+
+function handleVideoVolume() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = `<i class="fas fa-volume-up"></i>`;
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`;
   }
 }
 
 function init() {
   playBtn.addEventListener("click", handleVideoPlay);
+  volumeBtn.addEventListener("click", handleVideoVolume);
 }
 
 videoContainer && init();
